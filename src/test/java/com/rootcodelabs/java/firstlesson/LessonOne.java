7a0fdb697085b2e553f6f494eebd347d26d10ef7
Janesh Kodikara <janesh@pragmatictesters.com>
2022-08-04 12:13:18 +0530

    Add review comments to day 1 solutions

--- src/test/java/com/rootcodelabs/java/firstlesson/LessonOne.java
+++ src/test/java/com/rootcodelabs/java/firstlesson/LessonOne.java
@@ -3,14 +3,12 @@ package com.rootcodelabs.java.firstlesson;
 public class LessonOne {
 
     public static void main(String[] args) {
-
         String val1 = "10.5";
         String val2 = "12.5";
 
         float total = Float.parseFloat(val1) + Float.parseFloat(val2);
 
         System.out.println("Total is USD " + total);
-
     }
 
 }
--- src/test/java/com/rootcodelabs/java/firstlesson/LessonThree.java
+++ src/test/java/com/rootcodelabs/java/firstlesson/LessonThree.java
@@ -7,8 +7,20 @@ public class LessonThree {
     public static void main(String[] args) {
 
         Faker faker = new Faker();
+        //TODO: Are we getting the expected phone number format. Please refer to my solution
         String mobileNumber = faker.phoneNumber().phoneNumber();
         System.out.println(String.format("Mobile no. %s", mobileNumber));
+
+        String newMobileNumber = getMobileNumber("071", 10);
+        System.out.println("newMobileNumber = " + newMobileNumber);
     }
+
+
+    public static String getMobileNumber(String prefix, int length) {
+        Faker faker = new Faker();
+        String suffix = faker.number().digits(length - prefix.length());
+        return String.format("%s%s", prefix, suffix);
+    }
+
 
 }
